eccb8fc3b74a4da19065822409b1c2ff5f6b1837
WanderVafla <procenkovladik1@gmail.com>
2026-03-25 09:07:25 +0100

    complited ex: maths.js

--- src/basics/maths.js
+++ src/basics/maths.js
@@ -4,14 +4,24 @@
  */
 export function computeSphereVolume(diameter) {
   // Write your code here
+  if (Number.isFinite(diameter) && diameter >= 0) {
+      return (Math.PI/6) *Math.pow(diameter, 3);
+  } else {
+    throw new Error()
+  }
 }
-
+// computeSphereVolume(10)
 /**
  * @param {number} n
  * @return {number} number rounded to one decimal
  */
 export function roundNumberToOneDecimals(n) {
   // Write your code here
+  if (Number.isFinite(n)) {
+    return Math.round(n * 10) / 10
+  } else {
+    throw new Error()
+  }
 }
 
 /**
@@ -20,6 +30,16 @@ export function roundNumberToOneDecimals(n) {
  */
 export function computeAverage(grades) {
   // Write your code here
+  if (Array.isArray(grades)) {
+    for (const i of grades) {
+      if (!Number.isFinite(i)) {
+        throw new Error();
+      }
+    }
+    return grades.reduce((acc, n) => acc + n) / grades.length;
+  } else {
+    throw new Error();
+  }
 }
 
 /**
@@ -28,4 +48,14 @@ export function computeAverage(grades) {
  */
 export function roundedAverage(grades) {
   // Write your code here
+   if (Array.isArray(grades)) {
+    for (const i of grades) {
+      if (!Number.isFinite(i)) {
+        throw new Error();
+      }
+    }
+    return Math.round((grades.reduce((acc, n) => acc + n) / grades.length) * 10) / 10;
+  } else {
+    throw new Error();
+  }
 }
